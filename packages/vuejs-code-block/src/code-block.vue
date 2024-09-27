83ff913b9f5e5a71db365aa312816cccd7817232
Hetari <hetari4py@gmail.com>
2024-09-27 23:07:46 +0300

    init

---
+++ packages/vuejs-code-block/src/code-block.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>test</div>
+  {{ code }}
+</template>
+
+<script setup lang="ts">
+  // @ts-nocheck
+  import { ref } from 'vue';
+
+  defineProps({
+    code: {
+      type: String,
+      required: true
+    }
+  });
+</script>
