9816231cb2b939de58aaf7e4811df3a80db2f38f
Hetari <hetari4py@gmail.com>
2024-09-30 19:38:03 +0300

    add docs into the package

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,52 @@
+import { defineConfig } from 'vitepress';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: '/vuejs-code-block/',
+  lang: 'en-US',
+  title: 'Vuejs Code Block',
+  description: 'Unstyled UI components to build powerful code blocks in Vue3',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/images/logo.svg',
+    nav: [
+      {
+        text: 'Home',
+        link: '/',
+        activeMatch: '/home',
+        target: '_self',
+        rel: 'noopener'
+      },
+      { text: 'Example', link: '/example' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Example',
+        items: [
+          { text: 'Markdown Examples', link: '/example' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://hetari/'
+      },
+      {
+        icon: 'x',
+        link: 'https://x.com/4hetari'
+      },
+      {
+        icon: 'linkedin',
+        link: 'https://www.linkedin.com/in/4hetari'
+      },
+      {
+        icon: 'npm',
+        link: 'https://www.npmjs.com/package/vuejs-code-block'
+      }
+    ]
+  }
+});
---
+++ docs/index.md
@@ -0,0 +1,26 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Vuejs Code Block"
+  text: "Unstyled UI components to build powerful code blocks in Vue3"
+  tagline: Share it smoothly
+  image:
+    src: /images/logo.png
+    alt: Vuejs Code Block Logo
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: TypeScript
+    details: Fully type-safe components, made with 💙 TypeScript.
+  - title: Unstyled
+    details: Unstyled UI components to build powerful code blocks.
+---
+
